6232885dfb3cfa452fbd9a5033f3e57885f3e5a7
Grenish rai <mrcoder2033d@gmail.com>
2026-08-20 19:37:58 +0530

    chore: scaffold project manifest and entrypoint

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+mod gui;
+mod cli;
+
+use clap::Parser;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli_args = cli::Cli::parse();
+
+    if cli_args.gui {
+        gui::run_gui()?;
+    } else {
+        cli::handle_cli(cli_args)?;
+    }
+
+    Ok(())
+}
